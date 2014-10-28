dfc1311535df6a93552dfee733e618d00e3cbaa5
Stephen Feather <sfeather@gmail.com>
2014-10-28 16:17:06 -0400

    use label to check bluetooth radio status and turn on broadcasting if available

--- TiPro-6-BLERegions/app/controllers/index.js
+++ TiPro-6-BLERegions/app/controllers/index.js
@@ -4,6 +4,15 @@ if (OS_IOS) {
     beacons = require('org.beuckman.tibeacons');
     beacons.addEventListener('bluetoothStatus', function(e) {
         console.log(e);
+        if (e.status === 'on') {
+            beacons.startAdvertisingBeacon({
+                uuid: "F60E144E-F138-42AA-B362-2E91DE91C7B5",
+                identifier: "Titanium in Practice Beacon Test",
+                major: 100,
+                minor: 1
+            });
+
+        }
     });
 } else {
     beacons = require('com.liferay.beacons');
