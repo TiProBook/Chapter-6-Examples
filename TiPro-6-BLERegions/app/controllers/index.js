ecf7b4df0fbf131da85181929af5e669e4506343
Stephen Feather <sfeather@gmail.com>
2014-11-10 15:00:57 -0500

    Added monitor and advertising

--- TiPro-6-BLERegions/app/controllers/index.js
+++ TiPro-6-BLERegions/app/controllers/index.js
@@ -1,16 +1,19 @@
 var beacons;
 
+var beaconObject = {
+    uuid: "F60E144E-F138-42AA-B362-2E91DE91C7B5",
+    identifier: "Titanium in Practice Beacon Test",
+    major: 100,
+    minor: 1
+};
+
 if (OS_IOS) {
     beacons = require('org.beuckman.tibeacons');
     beacons.addEventListener('bluetoothStatus', function(e) {
         console.log(e);
         if (e.status === 'on') {
-            beacons.startAdvertisingBeacon({
-                uuid: "F60E144E-F138-42AA-B362-2E91DE91C7B5",
-                identifier: "Titanium in Practice Beacon Test",
-                major: 100,
-                minor: 1
-            });
+            beacons.startAdvertisingBeacon(beaconObject);
+        } else {
 
         }
     });
@@ -18,8 +21,20 @@ if (OS_IOS) {
     beacons = require('com.liferay.beacons');
 }
 
-$.label.addEventListener('touchstart', function(e) {
+$.broadcastButton.addEventListener('click', function(e) {
     beacons.requestBluetoothStatus();
 });
 
-$.index.open();
+$.requestButton.addEventListener('click', function(e){
+	Ti.Geolocation.requestAuthorization(Ti.Geolocation.AUTHORIZATION_ALWAYS);
+});
+
+$.monitorButton.addEventListener('click', function(e) {
+    beacons.startMonitoringForRegion({
+        uuid: "F60E144E-F138-42AA-B362-2E91DE91C7B5",
+        identifier: "Test Region 1"
+    });
+
+});
+
+$.wrapper.open();
